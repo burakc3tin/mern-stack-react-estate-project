132d1c8ccf42e89910f9a6b669920aca3b7ff26d
burakc3tin <burakcetin9724@gmail.com>
2023-12-07 14:18:26 +0300

    install reactjs with vite and tailwind css and create first template

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-lime-600 font-serif'>App</div>
+    <div className='text-lime-600 font-serif'>
+      App
+      </div>
   )
 }
